f771ce567ac64e9950bd0cd6ee170a51ec73804d
Tinnguyen13-7 <nguyentin130705@gmail.com>
2026-05-25 11:18:39 +0700

    implement task 1: AppConfig annotations

---
+++ slot5/Chapter2_ex1/src/main/java/hsf302/lab02/configs/AppConfig.java
@@ -0,0 +1,11 @@
+package hsf302.lab02.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = "hsf302.lab02")
+@EnableAspectJAutoProxy
+public class AppConfig {
+}
