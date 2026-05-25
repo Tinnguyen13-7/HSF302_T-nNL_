cb784fd589c8db1444af394da692c23eb0e5db61
Tinnguyen13-7 <nguyentin130705@gmail.com>
2026-05-25 11:29:08 +0700

    implement task 4: LifecycleBean callbacks

---
+++ slot5/HSF302_TraLTB_Chapter2_SpringTest/src/main/java/hsf302/lab02/components/LifecycleBean.java
@@ -0,0 +1,26 @@
+package hsf302.lab02.components;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LifecycleBean {
+    private boolean initialized = false;
+
+    @PostConstruct
+    public void init() {
+        initialized = true;
+        System.out.println("[LIFECYCLE] LifecycleBean initialized");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        initialized = false;
+        System.out.println("[LIFECYCLE] LifecycleBean destroyed");
+    }
+
+    public boolean isInitialized() {
+        return initialized;
+    }
+}
