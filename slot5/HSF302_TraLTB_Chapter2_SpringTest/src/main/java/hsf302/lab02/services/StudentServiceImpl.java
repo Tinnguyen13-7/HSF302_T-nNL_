da4fd3d370fa3f1a0839362deac10bb1d29dcc56
Tinnguyen13-7 <nguyentin130705@gmail.com>
2026-05-25 11:28:55 +0700

    implement task 3: StudentServiceImpl

---
+++ slot5/HSF302_TraLTB_Chapter2_SpringTest/src/main/java/hsf302/lab02/services/StudentServiceImpl.java
@@ -0,0 +1,17 @@
+package hsf302.lab02.services;
+
+import hsf302.lab02.pojo.Student;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Override
+    public void addStudent(Student student) {
+        System.out.println("Added student: " + student.getName());
+    }
+
+    @Override
+    public String getStudentInfo(Student student) {
+        return student.toString();
+    }
+}
